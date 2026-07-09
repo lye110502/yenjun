18184a2f4122f01be35c7e69bcac14a544445e4d
lye110502 <lye110502@gmail.com>
2026-07-09 13:41:05 +0900

    Delete sw.js

--- sw.js
+++
@@ -1,3 +0,0 @@
-const CACHE='exchange-diary-v3';
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','index.html','style.css','script.js','manifest.json']))));
-self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
